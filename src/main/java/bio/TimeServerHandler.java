d70305e4fac1e19774989b8fb6f67a6b453d6406
Administrator <Administrator@USER-20160801KF>
2017-05-31 11:30:50 +0800

    1

---
+++ src/main/java/bio/TimeServerHandler.java
@@ -0,0 +1,61 @@
+package bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class TimeServerHandler implements Runnable{
+
+	private Socket socket;
+	
+	public TimeServerHandler(Socket socket) {
+		this.socket = socket;
+	}
+	@Override
+	public void run() {
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			// 定义输入输出流
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			String currentTime = null;
+			String body = null;
+			for(;;){
+				body = in.readLine();
+				if(body == null)
+					break;
+				System.out.println("the time server receive order : "+body);
+				//设置查询命令
+				currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)? new Date(System.currentTimeMillis()).toString():
+					"BAD ORDER";
+			}
+			out.println(currentTime);
+		} catch (Exception e) {
+			if(in != null){
+				try {
+					in.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			if(out !=null){
+				out.close();
+				out = null;
+			}
+			if(socket != null){
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			socket = null;
+		}
+	}
+
+}
