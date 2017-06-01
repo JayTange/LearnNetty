a1932d2afa38fe31fbbfeb582dff755bee8b133e
唐杰 <@MrTangbook>
2017-06-01 18:48:41 +0800

    1

--- src/main/java/bio/client/TimeClient.java
+++ src/main/java/bio/client/TimeClient.java
@@ -51,7 +51,7 @@ public class TimeClient {
 	}
 	
 	public static void main(String args[]){
-		for(int i = 0;i<5000;i++){
+		for(int i = 0;i<5;i++){
 			new Thread(new Runnable() {
 				
 				@Override
